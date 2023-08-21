35c1cb6c545d8f6a598c959b4704ff014cc995d6
Yakunin <alexyakun@gmail.com>
2023-08-21 11:46:00 +0300

    feat: new main class

--- src/main/java/ru/mpei/SecondMain.java
+++ src/main/java/ru/mpei/SecondMain.java
@@ -2,6 +2,6 @@ package ru.mpei;
 
 public class SecondMain {
     public static void main(String[] args) {
-        System.out.println('new sout');
+        System.out.println("new sout");
     }
 }
