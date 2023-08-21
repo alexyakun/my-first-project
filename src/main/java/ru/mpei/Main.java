e43782923077b328a0524a92452bbde4cd7af4d3
Yakunin <alexyakun@gmail.com>
2023-08-21 11:35:33 +0300

    new

--- src/main/java/ru/mpei/Main.java
+++ src/main/java/ru/mpei/Main.java
@@ -3,7 +3,7 @@ package ru.mpei;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
-        System.out.println("Hellodds world!");
+        System.out.println("Helloddjhjs world!");
 
     }
 }
